253db1ef1238e79307b9e608ae203098c15aff02
Tristan Engel <121832013+trengel@users.noreply.github.com>
2023-05-10 17:13:05 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 SECRET_NUMBER=$((1 + RANDOM % 1000))
+
 echo -n "Enter your username: "; read USERNAME
-echo $USERNAME
+REGISTERED_USER=$($PSQL "SELECT username FROM user_data WHERE username='$USERNAME'")
+if [[ -z $REGISTERED_USER ]]; then
+  $PSQL "INSERT INTO user_data(username) VALUES('$USERNAME')"
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM user_data WHERE username='$REGISTERED_USER'")
+  BEST_GAME=$($PSQL "SELECT best_game FROM user_data WHERE username='$REGISTERED_USER'")
+  echo "Welcome back, $REGISTERED_USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
