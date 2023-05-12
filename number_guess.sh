d1918a28953c62f435a7a5637a9c287dd892339b
Tristan Engel <121832013+trengel@users.noreply.github.com>
2023-05-12 16:14:09 -0400

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -4,6 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUMBER=$(( 1 + RANDOM % 1000 ))
 NUMBER_OF_GUESSES=0
 
+# user "login"
 echo -e "\nEnter your username:"; read USERNAME
 REGISTERED_USER=$($PSQL "SELECT username FROM user_data WHERE username='$USERNAME'")
 if [[ -z $REGISTERED_USER ]]; then
@@ -15,26 +16,28 @@ else
   echo -e "\nWelcome back, $REGISTERED_USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
+# game loop
 echo -e "\nGuess the secret number between 1 and 1000:"; read GUESS
 until [[ $GUESS == $SECRET_NUMBER ]]; do
   if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
-    (( NUMBER_OF_GUESSES++ ))
+    # (( NUMBER_OF_GUESSES++ ))
     echo -e "\nThat is not an integer, guess again:"
     read GUESS
-  elif (( $GUESS < $SECRET_NUMBER )); then
+  elif [[ $GUESS -lt $SECRET_NUMBER ]]; then
     (( NUMBER_OF_GUESSES++ ))
     echo -e "\nIt's higher than that, guess again:"
     read GUESS
-  elif (( $GUESS > $SECRET_NUMBER )); then
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
     (( NUMBER_OF_GUESSES++ ))
     echo -e "\nIt's lower than that, guess again:"
     read GUESS
   fi
 done
 
+# user successfully guesses the number
 (( NUMBER_OF_GUESSES++ ))
 GET_GUESSES=$($PSQL "SELECT best_game FROM user_data where username='$USERNAME'")
-if (( $NUMBER_OF_GUESSES < $GET_GUESSES )) || (( $GET_GUESSES == 0 )); then
+if [[ $NUMBER_OF_GUESSES -lt $GET_GUESSES ]] || [[ $GET_GUESSES -eq 0 ]]; then
   ADD_GUESSES=$($PSQL "UPDATE user_data SET best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'")
 fi
 ADD_GAMES_PLAYED=$($PSQL "UPDATE user_data SET games_played=games_played+1 WHERE username='$USERNAME'")
